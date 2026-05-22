ce6f0818675bff082471c01d757430934cb0be6a
yassinemarzouki <92584248+yassinemarzouki@users.noreply.github.com>
2026-05-22 16:43:21 +0100

    fix:netlify problm

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,9 @@ import { ProfilePageComponent } from './features/profile/profile-page/profile-pa
 import { CoursesPageComponent } from './features/courses/courses-page/courses-page.component';
 
 export const routes: Routes = [
-    { path: 'auth', component: AuthPageComponent },
+    { path: '', component: AuthPageComponent },
     { path: 'courses', component: CoursesPageComponent },
     { path: 'profile', component: ProfilePageComponent },
-  // Redirection par défaut vers auth
-  { path: '', redirectTo: 'auth', pathMatch: 'full' }
+  // Redirection par défaut vers accueil
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
